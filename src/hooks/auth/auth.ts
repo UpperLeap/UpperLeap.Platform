c4ee95748aeeada5ea4f6a234e259a3e8d0f1f0c
sicada <youssefkhaled3301@gmail.com>
2024-09-14 04:30:47 +0300

    refactor: move auth file

---
+++ src/hooks/auth/auth.ts
@@ -0,0 +1,31 @@
+"use server";
+
+import { AuthResponse } from "@/types/globals";
+import { cookies } from "next/headers";
+import { getIronSession } from "iron-session";
+import { SessionData, sessionOptions } from "@/utils/ironSessionOptions";
+
+export async function setAuthenticationCookie(data: AuthResponse) {
+  const session = await getIronSession<SessionData>(cookies(), sessionOptions);
+
+  session.accessToken = data.accessToken;
+  session.refreshToken = data.refreshToken;
+  session.isLoggedIn = true;
+
+  await session.save();
+}
+
+export async function getAuthenticationCookies() {
+  const session = await getIronSession<SessionData>(cookies(), sessionOptions);
+
+  return {
+    accessToken: session.accessToken,
+    refreshToken: session.refreshToken,
+  };
+}
+
+export async function deleteAuthenticationCookies() {
+  const session = await getIronSession<SessionData>(cookies(), sessionOptions);
+
+  session.destroy();
+}

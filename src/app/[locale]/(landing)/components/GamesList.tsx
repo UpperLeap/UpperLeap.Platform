e97f250a2d5059f5c3cc1d25f4edee3542032136
sicada <youssefkhaled3301@gmail.com>
2024-09-15 00:33:46 +0300

    feat: link gamesList to api

--- src/app/[locale]/(landing)/components/GamesList.tsx
+++ src/app/[locale]/(landing)/components/GamesList.tsx
@@ -1,42 +1,26 @@
-"use client";
+import RequestError from "@/components/shared/RequestError";
+import { getGames } from "@/services/games";
 import Image from "next/image";
 import Link from "next/link";
 
-const GamesList = () => {
-  const games = [
-    {
-      name: "Valorant",
-      path: "/game/valorant",
-      imageUrl:
-        "https://blitz-cdn.blitz.gg/blitz/ui/img/game-cover/val-min.webp",
-    },
-    {
-      name: "League of Legends",
-      path: "/game/lol",
-      imageUrl:
-        "https://blitz-cdn.blitz.gg/blitz/ui/img/game-cover/lol-min.webp",
-    },
-    {
-      name: "Deadlock",
-      path: "/game/deadlock",
-      imageUrl:
-        "https://blitz-cdn.blitz.gg/blitz/ui/img/game-cover/deadlock.webp",
-    },
-    {
-      name: "Counter-Strike 2",
-      path: "/game/cs2",
-      imageUrl:
-        "https://blitz-cdn.blitz.gg/blitz/ui/img/game-cover/cs2-min.webp",
-    },
-  ];
+const GamesList = async () => {
+  let games;
+
+  try {
+    games = await getGames();
+  } catch (error) {
+    return <RequestError />;
+  }
 
   return (
-    <div
-      className="max-w-medium mx-auto mt-10 arc-wrapper px-5"
-    >
+    <div className="max-w-medium mx-auto mt-10 arc-wrapper px-5">
       <div className="flex items-center gap-5 justify-around flex-wrap">
         {games.map((game, i) => (
-          <Link key={i} className="flex flex-col gap-4" href={game.path}>
+          <Link
+            key={i}
+            className="flex flex-col gap-4"
+            href={`/games/${game.id}`}
+          >
             <div className="game-card-wrapper">
               <Image
                 className="game-logo"
@@ -44,7 +28,8 @@ const GamesList = () => {
                 loading="lazy"
                 alt={`${game.name} logo`}
                 width={190}
-                height={253.3}
+                height={250}
+                style={{ width: "auto", height: "auto" }}
               />
             </div>
             <p className="text-left text-foreground font-bold">{game.name}</p>

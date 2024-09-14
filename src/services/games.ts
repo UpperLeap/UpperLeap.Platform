90dc9121d4705cffc1623a425aef03e190c9b5a1
sicada <youssefkhaled3301@gmail.com>
2024-09-15 00:41:40 +0300

    feat: get games service created

---
+++ src/services/games.ts
@@ -0,0 +1,21 @@
+import { BASE_URL } from "@/constants/api";
+import { BasicGame } from "@/types/game";
+
+export async function getGames() {
+  const response = await fetch(`${BASE_URL}/games`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  const data = await response.json();
+
+  if (!response.ok) {
+    throw new Error(data.message || "Failed to fetch games", {
+      cause: data,
+    });
+  }
+
+  return data as BasicGame[];
+}

35cfe37ea5730db8917fb34ed4f2453ce2e9d197
sicada <youssefkhaled3301@gmail.com>
2024-09-17 22:38:45 +0300

    feat: add landing components to game page

--- src/app/[locale]/(pages-layout)/[gameName]/layout.tsx
+++ src/app/[locale]/(pages-layout)/[gameName]/layout.tsx
@@ -1,17 +1,28 @@
 import React from "react";
 import GameNav from "./components/GameNav";
+import Services from "../(landing)/components/Services";
+import Faq from "../(landing)/components/Faq";
+import Disclaimer from "./components/Disclaimer";
+import GameHeader from "./components/GameHeader";
 
 export default function GameLayout({
   children,
-  params,
+  params: { gameName },
 }: {
   children: React.ReactNode;
-  params: { locale: string };
+  params: { gameName: string; locale: string };
 }) {
+
   return (
-    <section>
+    <>
       <GameNav />
-      <div className="z-[1] relative max-w-large mx-auto px-10 mt-10">{children}</div>
-    </section>
+      <div className="z-[1] relative max-w-large mx-auto px-10 mt-10 mobile:px-5 flex flex-col gap-14">
+        <GameHeader gameName={gameName} />
+        {children}
+      </div>
+      <Services />
+      <Faq />
+      <Disclaimer gameName={gameName} />
+    </>
   );
 }

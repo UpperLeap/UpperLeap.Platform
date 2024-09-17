f5d3c14e475b4df642695c88f104126003c9c28c
sicada <youssefkhaled3301@gmail.com>
2024-09-17 22:41:22 +0300

    refactor: add boosting list to separate component

--- src/app/[locale]/(pages-layout)/[gameName]/page.tsx
+++ src/app/[locale]/(pages-layout)/[gameName]/page.tsx
@@ -1,40 +1,30 @@
 import { getTranslations } from "next-intl/server";
-import Image from "next/image";
 import { notFound } from "next/navigation";
+import { gamesData } from "@/data/gamesData";
+import BoostingList from "./components/BoostingList";
 
 export default async function GamePage({
-  params: { gameName },
+  params: { gameName, locale },
 }: {
-  params: { gameName: string };
+  params: { gameName: string; locale: string };
 }) {
-  let gameData;
-
-  try {
-    gameData = await import(`../../../../data/${gameName}.ts`);
-    gameData = gameData.default;
-  } catch (error) {
+  const gameData = gamesData[gameName as keyof typeof gamesData];
+  if (!gameData) {
     notFound();
   }
 
   const t = await getTranslations();
 
   return (
-    <div className="min-h-[55vh]">
-      <div className="flex items-center gap-5">
-        <div className="p-2 rounded-full bg-background-secondary">
-          <Image
-            src={gameData?.logo}
-            alt={""}
-            width={45}
-            height={45}
-            loading="lazy"
-          />
-        </div>
-        <div>
-          <h1 className="text-2xl font-bold">{t(gameData?.mainTitle)}</h1>
-          <p className="text-sm text-gray-500">{t(gameData?.subTitle)}</p>
-        </div>
-      </div>
+    <div>
+      <h3 className="text-2xl font-semibold text-foreground mb-4">
+        {t("gameProfile.boosting")}
+      </h3>
+      <BoostingList
+        boostingCategories={gameData?.boostingCategories}
+        gameName={gameName}
+        locale={locale}
+      />
     </div>
   );
 }

7f38bc1c7be991aa0ec07b1e17cbbb93d13d9011
sicada <youssefkhaled3301@gmail.com>
2024-09-17 22:43:51 +0300

    feat: new text added to localization

--- src/data/valorant.ts
+++ src/data/valorant.ts
@@ -1,5 +1,6 @@
 import { IoRocket } from "react-icons/io5";
 import { GiSpartanHelmet } from "react-icons/gi";
+import { IconType } from "react-icons/lib";
 
 export const valorant = {
   name: "Valorant",
@@ -7,6 +8,27 @@ export const valorant = {
   logo: "https://cdn.gameboost.com/games/valorant/logo/icon.svg",
   mainTitle: "gameProfile.valorant.mainTitle",
   subTitle: "gameProfile.valorant.subTitle",
+  boostingTitle: "gameProfile.valorant.boostingTitle",
+  boostingSubTitle: "gameProfile.valorant.boostingSubTitle",
+  main: {
+    title: "gameProfile.valorant.mainTitle",
+    subTitle: "gameProfile.valorant.subTitle",
+    image: "https://cdn.gameboost.com/games/valorant/logo/icon.svg",
+    icon: null,
+  },
+  boosting: {
+    title: "gameProfile.valorant.boostingTitle",
+    subTitle: "gameProfile.valorant.boostingSubTitle",
+    image: null,
+    icon: IoRocket,
+  },
+  "rank-boost": {
+    title: "gameProfile.valorant.rankBoostTitle",
+    subTitle: "gameProfile.valorant.rankBoostSubTitle",
+    image: "https://cdn.gameboost.com/boost-forms/icons/dark/VALRankBoost.svg",
+    icon: null,
+  },
+  boostingIcon: IoRocket,
   tabs: [
     {
       name: "gameProfile.boosting",
@@ -21,6 +43,41 @@ export const valorant = {
       isActive: false,
     },
   ],
+  visibleSections: ["boosting"],
+  boostingCategories: [
+    {
+      name: "gameProfile.rankBoost",
+      path: "rank-boost",
+      description: "gameProfile.rankBoostDescription",
+      image:
+        "https://cdn.gameboost.com/boost-forms/banners/val-rank-boost.webp",
+    },
+    {
+      name: "gameProfile.winBoost",
+      path: "win-boost",
+      description: "gameProfile.winBoostDescription",
+      image: "https://cdn.gameboost.com/boost-forms/banners/val-win-boost.webp",
+    },
+    {
+      name: "gameProfile.placementBoost",
+      path: "placement-boost",
+      description: "gameProfile.placementBoostDescription",
+      image:
+        "https://cdn.gameboost.com/boost-forms/banners/val-placements-boost.webp",
+    },
+    {
+      name: "gameProfile.unratedMatches",
+      path: "unrated-boost",
+      description: "gameProfile.unratedMatches",
+      image:
+        "https://cdn.gameboost.com/boost-forms/banners/val-unrated-boost.webp",
+    },
+  ],
+  disclaimer: "gameProfile.valorant.disclaimer",
 };
+
+export type BoostingCategories = (typeof valorant.boostingCategories)[number];
+
+export type PageHeaderData = typeof valorant.boosting;
 
 export default valorant;

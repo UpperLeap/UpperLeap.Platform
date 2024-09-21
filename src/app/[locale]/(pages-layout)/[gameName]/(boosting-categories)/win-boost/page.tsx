9e6affc90f3ab69d27de142d621f72eecefecd98
sicada <youssefkhaled3301@gmail.com>
2024-09-21 12:52:42 +0300

    feat: win boost page created

--- src/app/[locale]/(pages-layout)/[gameName]/(boosting-categories)/win-boost/page.tsx
+++ src/app/[locale]/(pages-layout)/[gameName]/(boosting-categories)/win-boost/page.tsx
@@ -1,5 +1,17 @@
-import React from "react";
+import Checkout from "../components/checkout/Checkout";
+import MatchesAmount from "../components/MatchesAmount";
+import RankPicker from "../components/RankPicker";
 
 export default function WinBoostPage() {
-  return <div>WinBoostPage</div>;
+  return (
+    <div className="grid grid-cols-5 gap-5 medium:grid-cols-1 medium:gap-x-0">
+      <div className="col-span-3 flex flex-col gap-5">
+        <RankPicker key={"current"} />
+        <MatchesAmount />
+      </div>
+      <div className="col-span-2">
+        <Checkout />
+      </div>
+    </div>
+  );
 }

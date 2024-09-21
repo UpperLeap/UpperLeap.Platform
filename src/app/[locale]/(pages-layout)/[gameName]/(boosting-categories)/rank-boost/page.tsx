d329e15201daeaa5b08652f99c9905ca7c5794f6
sicada <youssefkhaled3301@gmail.com>
2024-09-21 12:52:24 +0300

    feat: rank boost page created

--- src/app/[locale]/(pages-layout)/[gameName]/(boosting-categories)/rank-boost/page.tsx
+++ src/app/[locale]/(pages-layout)/[gameName]/(boosting-categories)/rank-boost/page.tsx
@@ -1,5 +1,16 @@
-import React from "react";
+import Checkout from "../components/checkout/Checkout";
+import RankPicker from "../components/RankPicker";
 
 export default function RankBoostPage() {
-  return <div>RankBoostPage</div>;
+  return (
+    <div className="grid grid-cols-5 gap-5 medium:grid-cols-1 medium:gap-x-0">
+      <div className="col-span-3 flex flex-col gap-5">
+        <RankPicker key={"current"} />
+        <RankPicker key={"desired"} isDesiredRank />
+      </div>
+      <div className="col-span-2">
+        <Checkout />
+      </div>
+    </div>
+  );
 }

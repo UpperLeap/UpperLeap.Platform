9ee2cf40f50cb703846517ee2eadcdf4cbcfe274
sicada <youssefkhaled3301@gmail.com>
2024-09-21 13:05:31 +0300

    feat: add get single game to games services

--- src/services/games.ts
+++ src/services/games.ts
@@ -19,3 +19,22 @@ export async function getGames() {
 
   return data as BasicGame[];
 }
+
+export async function getGame(gameName: string) {
+  const response = await fetch(`${BASE_URL}/games/${gameName}`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  const data = await response.json();
+
+  if (!response.ok) {
+    throw new Error(data.message || "Failed to fetch game data", {
+      cause: data,
+    });
+  }
+
+  return data as BasicGame;
+}

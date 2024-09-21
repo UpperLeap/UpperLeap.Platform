507eda60afa4ee85cc4b3c4f8473b20c85598522
sicada <youssefkhaled3301@gmail.com>
2024-09-21 17:15:09 +0300

    fix: fix discounted price size

--- src/app/[locale]/(pages-layout)/[gameName]/(boosting-categories)/components/checkout/Price.tsx
+++ src/app/[locale]/(pages-layout)/[gameName]/(boosting-categories)/components/checkout/Price.tsx
@@ -9,17 +9,19 @@ const Price = ({ priceData }: { priceData: PriceData | null }) => {
   return (
     <div className="flex items-end justify-between gap-6">
       <p>{t("gameProfile.totalPrice")}</p>
-      <div className="relative">
-        <span className="text-3xl text-foreground font-semibold">$</span>
-        <NumberTicker
-          className="font-bold text-4xl text-center bg-gradient-to-r from-foreground dark:from-[#C0C0C0] via-foreground dark:via-foreground dark:to-[#C0C0C0] to-foreground inline-block text-transparent bg-clip-text"
-          value={priceData?.subtotal || 0.0}
-        />
+      <div className="relative flex items-end gap-2">
         {priceData?.subtotal !== priceData?.priceBeforeDiscount && (
-          <del className=" text-warning-500 absolute bottom-0 -left-9">
+          <del className=" text-warning-500">
             ${priceData?.priceBeforeDiscount}
           </del>
         )}
+        <div>
+          <span className="text-3xl text-foreground font-semibold">$</span>
+          <NumberTicker
+            className="font-bold text-4xl text-center bg-gradient-to-r from-foreground dark:from-[#C0C0C0] via-foreground dark:via-foreground dark:to-[#C0C0C0] to-foreground inline-block text-transparent bg-clip-text"
+            value={priceData?.subtotal || 0.0}
+          />
+        </div>
       </div>
     </div>
   );

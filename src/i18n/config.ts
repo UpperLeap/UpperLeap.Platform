2e7bdfdd125467735cd1e4aaadbbfb63a1415667
sicada <youssefkhaled3301@gmail.com>
2024-10-02 02:07:25 +0300

    feat: add languages prefixes

--- src/i18n/config.ts
+++ src/i18n/config.ts
@@ -2,3 +2,8 @@ export type Locale = (typeof locales)[number];
 
 export const locales = ["en", "de"] as const;
 export const defaultLocale: Locale = "en";
+
+export const languagesPrefixes = {
+  en: "en-US",
+  de: "de-DE",
+};

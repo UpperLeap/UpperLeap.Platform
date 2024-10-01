15efcf5dc7815f9019575644ea5222b02bc03685
sicada <youssefkhaled3301@gmail.com>
2024-10-02 02:12:16 +0300

    feat: order service created

---
+++ src/services/orders.ts
@@ -0,0 +1,27 @@
+import { BASE_URL } from "@/constants/api";
+import { Order, OrdersResponse } from "@/types/order";
+import { getSession } from "@/utils/auth";
+
+export async function getOrders() {
+  const session = await getSession();
+  if (!session?.accessToken) return;
+  const accessToken = session?.accessToken;
+
+  const response = await fetch(`${BASE_URL}/orders`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${accessToken}`,
+    },
+  });
+
+  const data = await response.json();
+
+  if (!response.ok) {
+    throw new Error(data.message || "Failed to fetch user", {
+      cause: data,
+    });
+  }
+
+  return data as OrdersResponse;
+}

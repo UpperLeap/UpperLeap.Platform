8f22808b6b193f06b8b35317edd064d22e2254da
sicada <youssefkhaled3301@gmail.com>
2024-10-05 11:57:20 +0300

    feat: sizes added to placeholder

--- src/components/shared/Placeholder.tsx
+++ src/components/shared/Placeholder.tsx
@@ -1,17 +1,45 @@
+import { cn } from "@/utils/utils";
+
 const Placeholder = ({
   icon,
   title,
   description,
+  size = "lg",
 }: {
   icon: JSX.Element;
   title: string;
   description: string;
+  size?: "lg" | "md" | "sm";
 }) => {
+  const fontSize = {
+    lg: {
+      icon: "text-5xl",
+      title: "text-xl",
+      description: "max-w-sm",
+    },
+    md: {
+      icon: "text-3xl",
+      title: "text-lg",
+      description: "max-w-sm",
+    },
+    sm: {
+      icon: "text-xl",
+      title: "text-base",
+      description: "max-w-sm text-sm",
+    },
+  };
+
   return (
     <div className="flex flex-col items-center">
-      <span className="mb-2 text-5xl">{icon}</span>
-      <span className="mb-1 text-xl font-semibold capitalize">{title}</span>
-      <span className="max-w-sm text-center">{description}</span>
+      <span className={cn("mb-2", fontSize[size].icon)}>{icon}</span>
+      <span
+        className={cn("mb-1 font-semibold capitalize", fontSize[size].title)}
+      >
+        {title}
+      </span>
+      <span className={cn("max-w-sm text-center", fontSize[size].description)}>
+        {description}
+      </span>
     </div>
   );
 };

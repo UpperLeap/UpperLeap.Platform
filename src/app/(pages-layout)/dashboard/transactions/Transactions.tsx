8f69bef3e769423711ade1cdc9212d44654783c3
sicada <youssefkhaled3301@gmail.com>
2024-10-06 20:07:13 +0300

    feat: transactions linked to api with intial server data

---
+++ src/app/(pages-layout)/dashboard/transactions/Transactions.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import AsyncDataWrapper from "@/components/shared/AsyncDataWrapper";
+import { useGet } from "@/hooks/api/useGet";
+import { Wallet } from "@/types/wallet";
+import { useTranslations } from "next-intl";
+import { useSearchParams } from "next/navigation";
+import TransactionsTable from "./components/TransactionsTable";
+import PageLimit from "../orders/components/filters/PageLimit";
+import TablePagination from "../orders/components/filters/TablePagination";
+import { IoSwapVertical } from "react-icons/io5";
+
+const Transactions = ({ walletData }: { walletData: Wallet }) => {
+  const t = useTranslations();
+  const searchParams = useSearchParams();
+
+  const { data, isLoading, isError, isSuccess } = useGet<Wallet>({
+    endpoint: `/authentication/me/wallet?${searchParams.toString()}`,
+    queryKey: ["wallet", searchParams.toString()],
+    queryOptions: {
+      staleTime: 0,
+      initialData: walletData,
+    },
+  });
+
+  return (
+    <>
+      <AsyncDataWrapper
+        isLoading={isLoading}
+        isError={isError}
+        isSuccess={isSuccess}
+        isEmpty={data && data?.transactions?.length === 0}
+        placeholderIcon={<IoSwapVertical />}
+        placeholderTitle={t("transactions.noTransactions")}
+        placeholderDescription={t("transactions.noTransactionsDescription")}
+      >
+        {data && <TransactionsTable transactions={data.transactions} />}
+      </AsyncDataWrapper>
+      <div className="flex items-center justify-between">
+        <PageLimit />
+        <TablePagination totalPages={1} />
+      </div>
+    </>
+  );
+};
+
+export default Transactions;

9c250461870b45bed775f87acd07ebfa3a1ef275
sicada <youssefkhaled3301@gmail.com>
2024-10-09 01:52:12 +0300

    feat: terms of services page created

---
+++ src/app/(pages-layout)/terms-of-service/page.tsx
@@ -0,0 +1,23 @@
+import { termsOfService } from "@/data/cms-markdown";
+import { useTranslations } from "next-intl";
+import { MDXRemote } from "next-mdx-remote/rsc";
+
+export default function TermsOfServicePage() {
+  const t = useTranslations();
+  return (
+    <section className="min-h-[63vh] relative z-[1] max-w-medium mx-auto px-5 py-10">
+      <div className="w-fit mx-auto text-center mt-20">
+        <p className="text-xs font-bold mb-5 text-primary">
+          {t("cms.lastUpdated")} Oct 8th, 2024
+        </p>
+        <h1 className="text-5xl font-bold text-foreground">
+          {t("cms.termsOfService")}
+        </h1>
+        <p className="max-w-4xl mt-2">{t("cms.termsOfServiceDescription")}</p>
+      </div>
+      <div className="mt-10">
+        <MDXRemote source={termsOfService} />
+      </div>
+    </section>
+  );
+}

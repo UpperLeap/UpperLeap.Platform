6b029e37f1964c463a737c1b75f7ebd6d19dfd47
sicada <youssefkhaled3301@gmail.com>
2024-10-09 01:49:05 +0300

    feat: about us page created

---
+++ src/app/(pages-layout)/about/page.tsx
@@ -0,0 +1,40 @@
+import { about } from "@/data/cms-markdown";
+import { useTranslations } from "next-intl";
+import { MDXRemote } from "next-mdx-remote/rsc";
+
+export default function AboutPage() {
+  const t = useTranslations();
+
+  // const components = {
+  //   h1: (props: any) => (
+  //     <h1 className="text-3xl font-bold text-foreground" {...props} />
+  //   ),
+  //   h2: (props: any) => (
+  //     <h2 className="text-2xl font-bold text-foreground" {...props} />
+  //   ),
+  //   h3: (props: any) => (
+  //     <h3 className="text-xl font-bold text-foreground" {...props} />
+  //   ),
+
+  //   p: (props: any) => (
+  //     <p className="" {...props} />
+  //   ),
+  // };
+
+  return (
+    <section className="min-h-[63vh] relative z-[1] max-w-medium mx-auto px-5 py-10">
+      <div className="w-fit mx-auto text-center mt-20">
+        <p className="text-xs font-bold mb-5 text-primary">
+          {t("cms.lastUpdated")} Oct 8th, 2024
+        </p>
+        <h1 className="text-5xl font-bold text-foreground">
+          {t("cms.aboutUs")}
+        </h1>
+        <p className="max-w-4xl mt-2">{t("cms.aboutUsDescription")}</p>
+      </div>
+      <div className="mt-10">
+        <MDXRemote source={about} />
+      </div>
+    </section>
+  );
+}

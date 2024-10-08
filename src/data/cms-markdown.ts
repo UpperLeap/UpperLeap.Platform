9ec5013123bf41d9f55bfdfd5a6fa139ebc3e0d8
sicada <youssefkhaled3301@gmail.com>
2024-10-09 01:52:39 +0300

    feat: cms pages content added to data

---
+++ src/data/cms-markdown.ts
@@ -0,0 +1,15 @@
+export const about = `
+# About Us
+`;
+
+export const termsOfService = `
+# Terms of Service
+`;
+
+export const privacyPolicy = `
+# Privacy Policy
+`;
+
+export const cookiesPolicy = `
+# Cookies Policy
+`;

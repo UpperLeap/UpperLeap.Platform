32546e8b97ab4af4599d68af82f12a50887cfa4a
sicada <youssefkhaled3301@gmail.com>
2024-10-11 02:47:58 +0300

    feat: blog type created

---
+++ src/types/blog.ts
@@ -0,0 +1,16 @@
+export type Blog = {
+  id: string;
+  authorId: string;
+  title: string;
+  slug: string;
+  content: string;
+  coverUrl: string;
+  views: number;
+  createdDate: string;
+  updatedDate: string;
+  author: {
+    id: string;
+    userName: string;
+    imageUrl: string;
+  };
+};

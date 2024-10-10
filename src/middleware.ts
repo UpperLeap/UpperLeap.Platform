4b244e6790e060a7e396a160f650ffff908f6825
sicada <youssefkhaled3301@gmail.com>
2024-10-11 02:45:59 +0300

    feat: send page url in req header

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,7 +8,7 @@ export default async function middleware(
   const url = request.nextUrl;
 
   // Add this check to prevent redirecting if we're already on the refresh-session route
-  if (!url.pathname.startsWith('/api/refresh-session')) {
+  if (!url.pathname.startsWith("/api/refresh-session")) {
     if (session?.exp && Date.now() >= session?.exp * 1000) {
       const refreshUrl = new URL("/api/refresh-session", request.url);
       refreshUrl.searchParams.set("redirectTo", url.pathname);
@@ -20,6 +20,7 @@ export default async function middleware(
   const response = NextResponse.next();
 
   response.headers.set("current-path", request.nextUrl.pathname);
+  response.headers.set("current-url", url.toString());
 
   return response;
 }

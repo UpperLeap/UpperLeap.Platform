b48fbe31ab3cd167471e21ce6927e4f1b16bc673
sicada <youssefkhaled3301@gmail.com>
2024-10-15 06:27:20 +0300

    fix: sync toaster color with  theme

--- src/providers/ToasterProvider.tsx
+++ src/providers/ToasterProvider.tsx
@@ -1,7 +1,12 @@
+"use client";
+
 import { TProvider } from "@/types/globals";
+import { useTheme } from "next-themes";
 import { DefaultToastOptions, Toaster } from "react-hot-toast";
 
 export default function ToasterProvider({ children }: TProvider) {
+  const { theme } = useTheme();
+
   const globalToasterOptions: DefaultToastOptions = {
     duration: 2000,
     style: {
@@ -10,15 +15,15 @@ export default function ToasterProvider({ children }: TProvider) {
       maxWidth: "unset",
       textAlign: "center",
       fontWeight: "bold",
-      color: "#fff",
-      backgroundColor: "#040307",
+      color: theme === "dark" ? "#fff" : "#000",
+      backgroundColor: theme === "dark" ? "#040307" : "#F9F8FC",
       borderRadius: "20px",
       boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.1)",
-      border: `2px solid #1a1a1a`,
+      border: `2px solid ${theme === "dark" ? "#1a1a1a" : "#f7f7f9"}`,
     },
     error: {
       style: {
-        backgroundColor: "#040307",
+        backgroundColor: theme === "dark" ? "#040307" : "#F9F8FC",
       },
     },
   };

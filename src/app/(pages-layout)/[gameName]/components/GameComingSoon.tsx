e02a31fdd01638d49bc495877797f88ce9126a5e
sicada <youssefkhaled3301@gmail.com>
2024-10-18 14:56:30 +0300

    fix: undefined icon error

--- src/app/(pages-layout)/[gameName]/components/GameComingSoon.tsx
+++ src/app/(pages-layout)/[gameName]/components/GameComingSoon.tsx
@@ -2,22 +2,28 @@ import { BasicGame } from "@/types/game";
 import { useTranslations } from "next-intl";
 import Image from "next/image";
 import GetNotifiedForm from "./GetNotifiedForm";
-
+import { IoGameControllerOutline } from "react-icons/io5";
 const GameComingSoon = ({ game }: { game: BasicGame }) => {
   const t = useTranslations();
+  console.log(game);
 
   return (
-    <div className="max-w-xl">
+    <div className="max-w-2xl">
       <div className="flex items-center gap-5 mb-14">
         <div className="p-3 rounded-full bg-background-secondary border-1 border-foreground-secondary/20">
-          <Image
-            src={game.iconUrl}
-            alt={`${game.name} logo`}
-            width={30}
-            height={30}
-            loading="lazy"
-            // className="invert dark:invert-0 grayscale"
-          />
+          {game.iconUrl ? (
+            <Image
+              src={game.iconUrl}
+              alt={`${game.name} logo`}
+              width={30}
+              height={30}
+              loading="lazy"
+            />
+          ) : (
+            <span>
+              <IoGameControllerOutline className="text-2xl text-foreground" />
+            </span>
+          )}
         </div>
         <h1 className="text-2xl text-foreground font-semibold flex items-center gap-1 flex-wrap">
           <span className="capitalize">{game?.name?.toLowerCase()}</span>

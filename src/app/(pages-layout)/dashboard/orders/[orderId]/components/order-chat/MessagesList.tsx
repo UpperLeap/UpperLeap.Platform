d8453da1ba38f9ff6cb1aff0b4d7daa210b7fa60
Mohammed Sayed <officialcrusher@hotmail.com>
2024-11-09 16:44:09 +0200

    feat: messages scroll to end

--- src/app/(pages-layout)/dashboard/orders/[orderId]/components/order-chat/MessagesList.tsx
+++ src/app/(pages-layout)/dashboard/orders/[orderId]/components/order-chat/MessagesList.tsx
@@ -5,6 +5,7 @@ import { Chat } from "@/types/chat";
 import useChat from "@/hooks/chat/useChat";
 import useChatConnector from "@/hooks/chat/useChatConnector";
 import useChatDataStore from "@/stores/chat";
+import { useEffect, useRef } from "react";
 
 const MessagesList = ({
   initialChatData,
@@ -16,9 +17,16 @@ const MessagesList = ({
   useChat(initialChatData);
   useChatConnector(initialChatData.id);
   const { messages } = useChatDataStore();
+  const messagesRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    if (messagesRef.current) {
+      messagesRef.current.scrollTop += 10000; 
+    }
+  }, [messages]);
 
   return (
-    <div className="flex flex-col w-full p-5 gap-2 max-h-[400px] overflow-y-auto">
+    <div ref={messagesRef} className="flex flex-col w-full p-5 gap-2 max-h-[400px] overflow-y-auto">
       {messages.map((message) => (
         <Message
           key={message.id}

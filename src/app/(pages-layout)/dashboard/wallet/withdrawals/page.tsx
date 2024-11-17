9a0cad043ff3f7fc4489335ab978c2e515ba951b
sicada <youssefkhaled3301@gmail.com>
2024-11-17 03:24:48 +0200

    feat: placeholder for empty withdrawals

--- src/app/(pages-layout)/dashboard/wallet/withdrawals/page.tsx
+++ src/app/(pages-layout)/dashboard/wallet/withdrawals/page.tsx
@@ -3,6 +3,10 @@ import { getWithdrawals } from "@/services/wallet";
 import { getSession } from "@/utils/auth";
 import { Metadata } from "next";
 import { redirect } from "next/navigation";
+import WithdrawalsTable from "./components/WithdrawalsTable";
+import Placeholder from "@/components/shared/Placeholder";
+import { GiTakeMyMoney } from "react-icons/gi";
+import { getTranslations } from "next-intl/server";
 
 export const metadata: Metadata = {
   title: "Withdrawals",
@@ -10,6 +14,7 @@ export const metadata: Metadata = {
 };
 
 export default async function WithdrawalsPage() {
+  const t = await getTranslations();
   const session = await getSession();
   const isBooster =
     session?.[
@@ -21,7 +26,16 @@ export default async function WithdrawalsPage() {
 
   if (!withdrawals) return <RequestError />;
 
-  console.log(withdrawals);
+  if (withdrawals.length === 0)
+    return (
+      <div className="mt-5">
+        <Placeholder
+          icon={<GiTakeMyMoney />}
+          title={t("transactions.noWithdrawals")}
+          description={t("transactions.noWithdrawalsDescription")}
+        />
+      </div>
+    );
 
-  return <div>Withdrawals</div>;
+  return <WithdrawalsTable withdrawals={withdrawals} />;
 }
